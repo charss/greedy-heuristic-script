426673c5b0e9f9eb3677703292538465cdd37726
Charles Reyes <charleskylereyes16@gmail.com>
2024-02-02 16:48:18 +0800

    Updates

--- automation2.py
+++ automation2.py
@@ -3,18 +3,16 @@ import sys
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Gets the file name for the terminal
 filename = sys.argv[1]
+print(filename)
 rows = []
 with open(filename, 'r') as file:
-    csvreader = csv.reader(file) # Reads the file and its content
+    csvreader = csv.reader(file)
     header = next(csvreader)
-
-    # Adds each row to the list
     for row in csvreader:
         rows.append(row)
 
-pasig_ch_coord = ["Pasig City Hall", '14.5595', '121.08129'] # Pasig Coordinates
+pasig_ch_coord = ["Pasig City Hall", '14.5595', '121.08129']
 
 
 coordinates = ''
@@ -24,12 +22,10 @@ for x in rows:
   poi_coord.append([float(x[2]), float(x[1])])
 
 URL = f"http://router.project-osrm.org/trip/v1/driving/121.081286,14.559503;{coordinates}121.081286,14.559503?overview=full&geometries=geojson"
-# Stores the data returned by the API
+# print(URL)
 r = requests.get(URL)
 
-print('Distance', r.json()['trips'][0]['distance'] / 1000, 'km')
 
-# Gets the POI sequence created by the API's algorithm
 waypoints = r.json()['waypoints']
 temp_rows = copy.deepcopy(rows)
 
@@ -40,12 +36,8 @@ arrangement = [''] * len(temp_rows)
 for index, x in enumerate(waypoints):
   arrangement[x['waypoint_index']] = temp_rows[index][0]
   
-print('\nOrder:')
-for x in arrangement:
-	print(f'-{x}')
-print()
+pprint.pprint(arrangement)
 
-# Creates a visual representation of the optimal route
 data = np.array(r.json()['trips'][0]['geometry']['coordinates'])
 poi_data = np.array(poi_coord)
 pasig_np = np.array([14.5595, 121.08129])
@@ -54,7 +46,7 @@ x, y = data.T
 poi_x, poi_y = poi_data.T
 pasig_x, pasig_y = pasig_np.T 
 plt.plot(121.08129,14.5595,'ro') 
-plt.plot(poi_x, poi_y, 'bo',  color='g')
+plt.plot(poi_x, poi_y, 'bo', color='g')
 plt.plot(x, y)
 plt.show()
 
